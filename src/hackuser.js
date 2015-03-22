6f68e68e23fa583692b9ade6394a1d6130c696fa
Suresh babu Isreal <sureshisreal@gmail.com>
2015-03-22 21:15:06 +0530

    Update hackuser.js
    Hi , the solution for the problem Hackathon  as follows

--- src/hackuser.js
+++ src/hackuser.js
@@ -15,5 +15,27 @@ hackAlgorithmToFindUsername(result['encrypted']);
 
 function hackAlgorithmToFindUsername(encrypterPass) {
   // complete the hack to find the username for any encrypted password from authorizer.js.
+  
+   var data = encrypterPass ;
+   if (!data)
   throw new Error("Missing hack to find username");
+   var a = data.split(""), n = a.length;   a.sort()  ;      //        console.log(a); sorted
+   
+   var current = null;
+    var count = 0;
+    var filter ;
+    for (var i = 0; i <= a.length; i++) {
+        if (a[i] != current) {
+            if (count > 0) {
+                filter = count/2;
+                   for (var j=0 ;j<filter ; j++)    {
+                       process.stdout.write(current);   
+                                                    }
+                           }
+                                current = a[i];
+                                count = 1;
+                         } else {
+                                 count++;
+                                 }
+                                        }
 }
