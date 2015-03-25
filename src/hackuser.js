70598cfc113d050f4b021c0cf83d59ab8226be75
Suresh babu Isreal <sureshisreal@gmail.com>
2015-03-25 15:45:00 +0530

    sureshisreal, single for loop

--- src/hackuser.js
+++ src/hackuser.js
@@ -18,25 +18,8 @@ function hackAlgorithmToFindUsername(encrypterPass) {
   
    if (!encrypterPass)
           throw new Error("Missing hack to find username");
-          if (!encrypterPass)
-       throw new Error("Missing hack to find username");
-   
-    var a = encrypterPass.split(""),n = a.length;   a.sort();    
-    var current = null,count = 0, filter;
-        for (var i = 0; i <= a.length; i++) {
-             if (a[i] != current) {
-                if (count > 0) {
-                    filter = count/2;
-                    recurssion (filter);
-                        }   current = a[i];
-                            count = 1 ;
-            } else {
-                    count++; }
-                                             } 
-    function recurssion(filter) {
-                 if(filter > 0) {
-                    recurssion(filter-1);
-                    process.stdout.write(current); }
-    return;                }
-}
+var a = encrypterPass.split(""), i ;   a.sort()  ;
+           for (i= 0; i < a.length; i = i + 2) {
+                process.stdout.write(a[i]); 
+                     }
 }
