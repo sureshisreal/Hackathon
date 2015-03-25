13833fdf2306ac9004235776a12ac7067a5ba3a0
Suresh babu Isreal <sureshisreal@gmail.com>
2015-03-25 13:28:15 +0530

    sureshisreal, removed inner for loop

--- src/hackuser.js
+++ src/hackuser.js
@@ -15,5 +15,28 @@ hackAlgorithmToFindUsername(result['encrypted']);
 
 function hackAlgorithmToFindUsername(encrypterPass) {
   // complete the hack to find the username for any encrypted password from authorizer.js.
-  throw new Error("Missing hack to find username");
+  
+   if (!encrypterPass)
+          throw new Error("Missing hack to find username");
+          if (!encrypterPass)
+       throw new Error("Missing hack to find username");
+   
+    var a = encrypterPass.split(""),n = a.length;   a.sort();    
+    var current = null,count = 0, filter;
+        for (var i = 0; i <= a.length; i++) {
+             if (a[i] != current) {
+                if (count > 0) {
+                    filter = count/2;
+                    recurssion (filter);
+                        }   current = a[i];
+                            count = 1 ;
+            } else {
+                    count++; }
+                                             } 
+    function recurssion(filter) {
+                 if(filter > 0) {
+                    recurssion(filter-1);
+                    process.stdout.write(current); }
+    return;                }
+}
 }
