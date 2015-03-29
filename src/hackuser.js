cbe546299b228cb97e3a61eca60bbed56802c107
Suresh babu Isreal <sureshisreal@gmail.com>
2015-03-29 18:34:32 +0530

    hackuser with Quick sort  updated
    Hi Vignesh, please find update code with Quicksort to reduce the time complexity.

--- src/hackuser.js
+++ src/hackuser.js
@@ -18,8 +18,43 @@ function hackAlgorithmToFindUsername(encrypterPass) {
   
    if (!encrypterPass)
           throw new Error("Missing hack to find username");
-var a = encrypterPass.split(""), i ;   a.sort()  ;
-           for (i= 0; i < a.length; i = i + 2) {
-                process.stdout.write(a[i]); 
-                     }
+var usernameErr = encrypterPass.split("");
+console.log(quickSort(usernameErr)); 
+
+      function partition(encrypters, low, high) {
+          var i = low, j = high + 1;
+              while (true) {
+                  while (encrypters[++i] < encrypters[low]) {
+                      if (i == high) 
+                          break;                      }
+              while (encrypters[--j] > encrypters[low]) {
+                  if (j == low) 
+                      break;                      }
+
+              if (i >= j) break;
+
+                    var encrypter = encrypters[i];
+                    encrypters[i] = encrypters[j];
+                    encrypters[j] = encrypter; 
+                          }
+    var value = encrypters[low];
+    encrypters[low] = encrypters[j];
+    encrypters[j] = value;
+    return j;
+                                            }
+
+    function sort(encrypters, low, high)        {
+         if (low >= high) return;
+             var index = partition(encrypters, low, high);
+             sort(encrypters, low, index - 1);
+             sort(encrypters, index + 1, high);  }
+
+    function quickSort(encrypters) {
+         sort(encrypters, 0, encrypters.length - 1);
+             var username= new Array();
+                  for (var k= 0; k < encrypters.length-1; k = k + 2) {
+                       username.push(encrypters[k]);                 } 
+                       username = username.join(""); 
+    return username ; 
+                                             }
 }
